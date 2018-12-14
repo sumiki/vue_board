58e32aa94d7a374ad2aca56cf2330d375883b4c9
Sumiki Ohsawa <sumikio@gmail.com>
2018-12-14 08:08:26 -0800

    vue material dialog for new card

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="board">
-    <h1>{{ msg }}</h1>
+    <h1 style="padding: 20px 0;">{{ msg }}</h1>
     <div class="board_inner">
       <div class="column" v-for="(column, i) in cards">
         <div class="column_inner" >
@@ -12,10 +12,24 @@
               <span v-if="i !== ( cards.length - 1 )"><a href="#" @click="(e) => { moveRight(e, i, j) }" >{{ `>` }}</a></span>
             </div>
           </div>
-          <div><a href="#" @click="(e) => add_card(e, i)">Add Card</a></div>
+          <div><a href="#" @click="(e) => add_card_dialog(e, i)">Add Card</a></div>
         </div>
       </div>
     </div>
+
+    <md-dialog
+      :md-active.sync="showDialog"
+    >
+      <md-dialog-title>Add New Card to {{ names[showDialogColumn] }}</md-dialog-title>
+      <md-dialog-content>
+        <input class="input" v-model="new_card" placeholder="CARD TEXTを入力してください。">
+      </md-dialog-content>
+      <md-dialog-actions>
+        <md-button class="md-primary" @click="handleCloseDialog">Close</md-button>
+        <md-button class="md-primary" @click="handleSaveDialog">Save</md-button>
+      </md-dialog-actions>
+    </md-dialog>
+
   </div>
 </template>
 
@@ -35,7 +49,10 @@ export default {
         ['default card b2'],
         ['default card c3'],
         ['default card d4']
-      ]
+      ],
+      showDialog: false,
+      showDialogColumn: null,
+      new_card: ''
     }
   },
   methods: {
@@ -45,6 +62,10 @@ export default {
       if( new_card && new_card.replace(' ', '') !== '' ){
         this.$data.cards[index].push( new_card )
       }
+    },
+    add_card_dialog: function(e, index){
+      this.$data.showDialog = true
+      this.$data.showDialogColumn = index
     },
     moveLeft: function(e, column_index, card_index){
       e.preventDefault()
@@ -57,6 +78,21 @@ export default {
       var move_card_value = this.$data.cards[column_index][card_index]
       this.$data.cards[column_index].splice(card_index, 1)
       this.$data.cards[column_index + 1].push( move_card_value )
+    },
+    handleCloseDialog: function(e){
+      if(e){
+        e.preventDefault()
+      }
+      this.$data.showDialog = false;
+      this.$data.showDialogColumn = null;
+      this.$data.new_card = '';
+    },
+    handleSaveDialog: function(e){
+      var new_card = this.$data.new_card
+      if( new_card && new_card.replace(' ', '') !== '' ){
+        this.$data.cards[ this.$data.showDialogColumn ].push( new_card )
+      }
+      this.handleCloseDialog(e)
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+//import VueMaterial from 'vue-material'
+import { MdButton, MdDialog } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 
+Vue.use(MdButton)
+Vue.use(MdDialog)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
